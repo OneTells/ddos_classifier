ca4032670829601a1cba6ddf7a7aa76231257f02
Egor <OneTells@ya.ru>
2023-12-18 15:24:49 +0500

    v1.6

--- main_with_flow2.py
+++ main_with_flow2.py
@@ -1,4 +1,3 @@
-# Data preprocessing and modeling
 import numpy as np
 from keras.models import Sequential
 from keras.src.layers import Dense
@@ -7,7 +6,7 @@ from keras.src.optimizers import Adam
 from modules.learning.environment import ClassifierEnv
 
 
-class Replay(object):
+class Memory(object):
 
     def __init__(self, env_dim, max_memory=100, discount=.9):
         self.max_memory = max_memory
@@ -47,23 +46,17 @@ class Replay(object):
         return inputs, targets
 
 
-loss_ = []
-reward_ = []
+loss_list = []
+reward_list = []
 
 
-def train(env, replayMemory, model, epochs, epsilon=.9, batch_size=10, set_size=10, timesteps=200, output=1):
-    set_hist = []
-    win_hist = []
-
-    win_cnt = 0
-    loss_over_set = 0.
-    frames_over_set = 0
-
+def train(env, memory: Memory, model, epochs, epsilon=.9, batch_size=10, timesteps=200):
     for i_episode in range(epochs):
-
         observation = env.reset()
-        loss = 0
-        reward__ = 0
+
+        loss_on_epochs = 0
+        reward_on_epochs = 0
+
         for t in range(timesteps):
             state_0 = np.array(observation, ndmin=2)
 
@@ -73,43 +66,18 @@ def train(env, replayMemory, model, epochs, epsilon=.9, batch_size=10, set_size=
                 action = np.argmax(model.predict(state_0)[0])
 
             observation, reward, done = env.step(action)
-            reward__ += reward
+            reward_on_epochs += reward
             state_1 = np.array(observation, ndmin=2)
 
-            replayMemory.remember([state_0, action, reward, state_1], done)
+            memory.remember([state_0, action, reward, state_1], done)
 
-            inputs, targets = replayMemory.get_batch(model, max_batch_size=batch_size)
+            inputs, targets = memory.get_batch(model, max_batch_size=batch_size)
 
-            batch_loss = model.train_on_batch(inputs, targets)
-            loss += batch_loss
+            loss = model.train_on_batch(inputs, targets)
+            loss_on_epochs += loss
 
-            frames_over_set += 1
-            loss_over_set += batch_loss
-
-            if done or t + 1 == timesteps:
-                if t + 1 == timesteps:
-                    win_cnt += 1
-                    win_hist.append(1)
-                else:
-                    win_hist.append(0)
-
-                break
-
-        if i_episode % set_size == set_size - 1:
-            if output > 0:
-                print("Episode finished after {} timesteps".format(t + 1))
-                print("Epoch {:03d}/{:03d} | Average Loss per frame over set: {:.4f} | Win count over past set: {}"
-                      .format(i_episode + 1, epochs, loss_over_set / frames_over_set, win_cnt))
-
-            win_cnt = 0
-            loss_over_set = 0.
-            frames_over_set = 0
-
-            set_hist.append(win_hist)
-            win_hist = []
-
-        loss_.append(loss)
-        reward_.append(reward__)
+        loss_list.append(loss_on_epochs)
+        reward_list.append(reward_on_epochs)
 
     env.reset()
 
@@ -123,14 +91,14 @@ def main():
     learning_rate = .005  # the multiplicative rate at which the weights of the model are shifted
     timesteps = 10  # length of each game (for Cartpole, ideally set this to between 100-200)
     epochs = 1  # (Amount of games played)
-    set_size = 10  # rate at which games are rendered and progress is reported
 
     env = ClassifierEnv()
 
     feature_dims = len(env.reset())
     num_actions = env.action_space.n
 
-    memory = Replay(max_memory=max_memory, discount=discount, env_dim=feature_dims)
+    memory = Memory(max_memory=max_memory, discount=discount, env_dim=feature_dims)
+
     model = Sequential()
     model.add(Dense(hidden_size, input_shape=(feature_dims,), activation='relu'))
     model.add(Dense(hidden_size, activation='relu'))
@@ -138,9 +106,9 @@ def main():
     model.compile(Adam(learning_rate), "mse")
 
     train(
-        env=env, replayMemory=memory, model=model, epochs=epochs,
-        epsilon=epsilon, batch_size=batch_size, set_size=set_size,
-        timesteps=timesteps, output=1
+        env=env, memory=memory, model=model, epochs=epochs,
+        epsilon=epsilon, batch_size=batch_size,
+        timesteps=timesteps
     )
 
     model.save_weights(f'model.weights.h5')
